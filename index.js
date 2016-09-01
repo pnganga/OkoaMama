61f1729b1047256561cb36251960c316d0a55c59
pnganga <pnganga05@gmail.com>
2016-09-01 13:26:18 -0700

    post method for api

--- index.js
+++ index.js
@@ -13,51 +13,51 @@ app.get('/', function(req, res){
 
 });
 
-// app.post('/ussd', function(req, res){
-// 	console.log(JSON.stringify(req.body));
-// 	console.log("Karibu Afya mama")
-// 	switch (req.body["text"]){
+app.post('/ussd', function(req, res){
+	console.log(JSON.stringify(req.body));
+	console.log("Karibu Afya mama")
+	switch (req.body["text"]){
 
-// 		case "":
-// 			message = "CON KARIBU AFYA MAMA\n";
-// 			message += "Tafadhali andika majina yako" 
-// 			res.send(message);
-// 			break;
-// 		case "":
-// 			message = "END Shukrani. Karibu Tena";
-// 			res.send(message);
-// 			break;
+		case "":
+			message = "CON KARIBU AFYA MAMA\n";
+			message += "Tafadhali andika majina yako" 
+			res.send(message);
+			break;
+		case "":
+			message = "END Shukrani. Karibu Tena";
+			res.send(message);
+			break;
 
-// 		case "":
-// 			message = "CON Kata Ndogo";
-// 			res.send(message);
-// 			break;
-// 		case "":
-// 			message = "CON Tafadhali chagua Umri wako\n";
-// 			message += "1. Chini ya miaka 18\n"
-// 			message += "2. Miaka 19 hadi 35\n"
-// 			message += "3. Miaka 36 na zaidi"
-// 			res.send(message);
-// 			break;
+		case "":
+			message = "CON Kata Ndogo";
+			res.send(message);
+			break;
+		case "":
+			message = "CON Tafadhali chagua Umri wako\n";
+			message += "1. Chini ya miaka 18\n"
+			message += "2. Miaka 19 hadi 35\n"
+			message += "3. Miaka 36 na zaidi"
+			res.send(message);
+			break;
 
-// 		case "1":
-// 			message = "CON Andika Kaunti yako";
-// 			res.send(message);
-// 			break;
+		case "1":
+			message = "CON Andika Kaunti yako";
+			res.send(message);
+			break;
 
-// 		case "2":
-// 			message = "CON Andika Kontie yako";
-// 			res.send(message);
-// 			break;
-// 		case "3":
-// 			message = "CON Kontie";
-// 			res.send(message);
-// 			break;
+		case "2":
+			message = "CON Andika Kontie yako";
+			res.send(message);
+			break;
+		case "3":
+			message = "CON Kontie";
+			res.send(message);
+			break;
 
 		
 			
-// 	}
-// });
+	}
+});
 
 
 app.listen(app.get('port'), function(){
